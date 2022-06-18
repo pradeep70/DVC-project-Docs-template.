48a0ed0f49565a5facf2301feaa9079cbe9630bb
Pradeep Kumar <pradeepyadav70@live.com>
2022-06-18 16:12:54 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 ## edit below variables as per your requirements -
 REPO_NAME = "REPO_NAME"
-AUTHOR_USER_NAME = "c17hawke"
+AUTHOR_USER_NAME = "pradeep70"
 SRC_REPO = "src"
 LIST_OF_REQUIREMENTS = []
 
@@ -18,9 +18,9 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
-    author_email="sunny.c17hawke@gmail.com",
+    author_email="pradeepyadav70@live.com",
     packages=[SRC_REPO],
     license="MIT",
-    python_requires=">=3.6",
+    python_requires=">=3.7",
     install_requires=LIST_OF_REQUIREMENTS
 )
